611f5670372bbab805072dcbc7f782b235d9ddbc
Youssef Younan <youssefunan966@outlook.com>
2021-11-01 19:34:29 +0200

    Adding Order Application Layers

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Presistence/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Presistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        //custom operations beside inherited
+
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    // validate CheckoutOrderCommand before handler "pre-processor"
+
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            //should be not empty and if empty return Message -- is required --
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+
+        }
+    }
+}
